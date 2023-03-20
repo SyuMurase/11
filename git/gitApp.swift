ae074b1d86da7bcc5f8599728a95b2ae03b721c1
Syu <mirzamrion@gmail.com>
2023-03-20 18:29:26 +0900

    Initial Commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by 村瀬嵩 on 2023/03/20.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
